e60f9a108de74c1efe9614ca0ce0bb92541183e8
Tarun Sabbineni <ts2615@imperial.ac.uk>
2017-07-15 09:54:39 +0100

    Displaying current driver standings by just name in a basic list

--- App.js
+++ App.js
@@ -1,21 +1,55 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ListView} from 'react-native';
+
+const myjson = require('./curr_drivers.json');
 
 export default class App extends React.Component {
+
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      }),
+    };
+  }
+
+  componentDidMount() {    
+    this.setState({
+      dataSource: this.state.dataSource.cloneWithRows(myjson.MRData.StandingsTable.StandingsLists[0].DriverStandings)
+    });
+  }
+
+  renderRow(standingCell) {
+      return (
+        <View>
+          <Text>{standingCell.Driver.familyName}</Text>
+        </View>
+      )
+  }
+
+  renderSectionHeader() {
+    return (
+      <View>
+          <Text>{myjson.MRData.StandingsTable.season}</Text>
+      </View>
+    )
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center',}}>
+       <ListView                     
+          dataSource={this.state.dataSource}
+          renderRow={this.renderRow.bind(this)}
+          renderHeader={this.renderSectionHeader.bind(this)}
+          enableEmptySections={true}
+        />
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
