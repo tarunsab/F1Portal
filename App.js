33f2b1a77dc3f12b215e4ee33157ee65886b7e9c
Tarun Sabbineni <ts2615@imperial.ac.uk>
2017-07-16 12:34:59 +0100

    Implemented a header and started list elem styling whilst adding points into the list elem display

--- App.js
+++ App.js
@@ -23,33 +23,81 @@ export default class App extends React.Component {
 
   renderRow(standingCell) {
       return (
-        <View>
+        <View style={styles.listElem}>
           <Text>{standingCell.Driver.familyName}</Text>
+          <Text>{standingCell.points}</Text>
         </View>
       )
   }
 
   renderSectionHeader() {
     return (
-      <View>
-          <Text>{myjson.MRData.StandingsTable.season}</Text>
+      <View style={styles.listHeader}>
+          <Text style={styles.listHeaderText}>{myjson.MRData.StandingsTable.season}</Text>
       </View>
     )
   }
 
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center',}}>
-       <ListView                     
-          dataSource={this.state.dataSource}
-          renderRow={this.renderRow.bind(this)}
-          renderHeader={this.renderSectionHeader.bind(this)}
-          enableEmptySections={true}
-        />
+      <View style={styles.container}>
+      
+        <View style={styles.header}>
+          <Text style={styles.headerText}>
+            F1 Portal
+          </Text>
+        </View>
+
+
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+         <ListView                     
+            dataSource={this.state.dataSource}
+            renderRow={this.renderRow.bind(this)}
+            renderHeader={this.renderSectionHeader.bind(this)}
+            enableEmptySections={true}
+          />
+        </View>
+
       </View>
     );
   }
 }
 
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header:{
+    backgroundColor: '#F44336',
+    paddingTop: 20,
+    paddingBottom: 10,
+    flexDirection: 'row'
+  },
+  headerText:{
+    color: '#fff',
+    textAlign: 'center',
+    fontWeight: 'normal',
+    flex: 1,
+    fontSize: 20,
+  },
+  listHeader:{
+    backgroundColor: '#F44336',
+    paddingTop: 20,
+    paddingBottom: 10,
+    flexDirection: 'row'
+  },
+  listHeaderText:{
+    color: '#fff',
+    textAlign: 'center',
+    fontWeight: 'normal',
+    flex: 1,
+    fontSize: 20,
+  },
+  listElem:{
+    backgroundColor: '#F44336',
+    paddingTop: 20,
+    paddingBottom: 10,
+    flexDirection: 'row'
+  }
+});
 
