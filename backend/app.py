cbf802b0f544f81175b370b0c708f06232476213
Tarun Sabbineni <ts2615@imperial.ac.uk>
2017-07-19 12:26:00 +0100

    All errors fixed for caching dur to bug in saving directory of drivers_data.json

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,10 @@
 from flask import Flask, jsonify
 from datetime import datetime
 from smartfile import BasicClient
-import requests, json
+
+import requests
+import json
+import shutil
 
 app = Flask(__name__)
 
@@ -10,52 +13,55 @@ s3Url = "https://s3.eu-west-2.amazonaws.com/f1portal/"
 api = BasicClient('tx1UMGxzsmsNYeygrykEcs9KFE58HY',
                   'vPgNHEE1LY1GOz626Qon8g4oerPrDm')
 
+
 @app.route('/')
 def homepage():
+    # with open('backend/driver_data.json', 'r+') as file:
+    #     api.upload('driver_data.json', file)
     return "Backend for F1 Portal app"
 
 
-#Obtaining the drivers standings data
+# Obtaining the drivers standings data
 @app.route('/get_drivers')
 def get_drivers():
-
-    #Obtain current cached file's expiry info
+    # Obtain current cached file's expiry info
     api.download('driver_data.json')
-    with open('driver_data.json', 'r') as file:
+    shutil.move("driver_data.json",
+                "backend/driver_data.json")
+    with open('backend/driver_data.json', 'r+') as file:
         driver_data = json.load(file)
 
     driver_data_expiry = driver_data["expiryDate"]
     refresh_date = datetime.strptime(driver_data_expiry, '%Y-%m-%d')
 
-    #Check to obtain from cache or refresh data based on cache's expiry info
-    if (datetime.now() >= refresh_date):
+    # Check to obtain from cache or refresh data based on cache's expiry info
+    if datetime.now() >= refresh_date:
         return get_drivers_refresh()
     else:
         print("Obtained driver standing details from cached file")
         return jsonify(driver_data)
 
 
-#Obtaining the drivers standings data from the Ergast API
+# Obtaining the drivers standings data from the Ergast API
 def get_drivers_refresh():
-
     drivers_standings = requests.get(apiUrl + 'driverStandings.json')
     data = drivers_standings.json()
 
-    #Adding expiry date to drivers standings json file to aid Caching
-    #TODO: Cache this to a json file too
+    # Adding expiry date to drivers standings json file to aid Caching
+    # TODO: Cache this to a json file too
     response = requests.get("http://ergast.com/api/f1/current.json")
     race_schedule = response.json()
 
-    racesJson = race_schedule["MRData"]["RaceTable"]["Races"]
+    races_json = race_schedule["MRData"]["RaceTable"]["Races"]
     curr_date = datetime.now()
-    for rJson in racesJson:
-        dateJson = rJson["date"]
-        race_date = datetime.strptime(dateJson, '%Y-%m-%d')
-        if (curr_date < race_date):
+    for r_json in races_json:
+        date_json = r_json["date"]
+        race_date = datetime.strptime(date_json, '%Y-%m-%d')
+        if curr_date < race_date:
             data["expiryDate"] = race_date.strftime('%Y-%m-%d')
             break
 
-    with open('driver_data.json', 'w') as file:
+    with open('backend/driver_data.json', 'w') as file:
         json.dump(data, file)
         api.upload('driver_data.json', file)
 
